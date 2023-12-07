c5ce3b7fc78276056d5d9ea5818a3ac7afc6c704
Thales Negrão <thalesnegrao@gmail.com>
2023-12-07 00:30:18 -0300

    Create tempconverter.go

---
+++ tempconverter.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const tempebulK float64 = 373
+
+func main() {
+
+	var c float64 = tempebulK - 273
+	fmt.Println("A temperatura de ebulição da agua em ºC é de:", c)
+
+}
